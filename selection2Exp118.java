0761c3b1121520efcf777813f9abfd1e77d01daa
chiquixs <nafisachiquita06@gmail.com>
2024-10-05 18:48:20 +0700

    modify

--- selection2Exp118.java
+++ selection2Exp118.java
@@ -10,9 +10,9 @@ public class selection2Exp118 {
         if (year%4 == 0) {
             if (year%100 != 0 ) {
                 System.out.println("leap year");
-            } if (year%400 == 0) {
+            } else if (year%400 == 0) {
                 System.out.println("leap year");
-            } else if (year%100 == 100) {
+            } else if (year%100 == 0) {
                 System.out.println("not a leap year");
             }
         } else 
